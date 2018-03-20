8f2d3138b65f7d442c1f9aea547da31a61c3c607
AlecGoldstein123 <34690976+AlecGoldstein123@users.noreply.github.com>
2018-03-20 14:47:57 -0400

    Add files via upload

---
+++ MadLibGames.py
@@ -0,0 +1,11 @@
+#Creation task game
+playGame=input("Hello! Welcome to Alec Libs free trial version."
+      "In this trial version you will only have 3 stories to have fun with."
+      "The only way to unlock more stories is to either pay $5.99 or complete a story in less than 10 seconds."
+      "So.. wanna play?: ") 
+
+while playGame != "no" and playGame != "yes":
+    print("Invalid command print yes or no")
+    playGame = input()
+
+print('hi')
